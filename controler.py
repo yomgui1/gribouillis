8c82e9edbc6312312511deb715954a93d55f963d
Guillaume Roguez <yomgui1@gmail.com>
2009-09-24 09:21:55 +0000

    Forget to commit the controler file

---
+++ controler.py
@@ -0,0 +1,215 @@
+###############################################################################
+# Copyright (c) 2009 Guillaume Roguez
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+__all__ = ('DrawControler', )
+
+from pymui import TABLETA_Pressure
+
+IECODE_UP_PREFIX = 0x80
+IECODE_LBUTTON   = 0x68
+IECODE_RBUTTON   = 0x69
+IECODE_MBUTTON   = 0x6A
+
+NM_WHEEL_UP      = 0x7a
+NM_WHEEL_DOWN    = 0x7b
+
+PRESSURE_MAX     = 0x7ffff800
+
+class Recorder:
+    def __init__(self):
+        self.evtlist = []
+
+    def Start(self, init=False):
+        if init:
+            self.evtlist = []
+
+    def Stop(self):
+        pass
+
+    def AddData(self, **data):
+        self.evtlist.append(data)
+
+
+class DrawControler(object):
+    MODE_IDLE = 0
+    MODE_DRAW = 1
+    MODE_DRAG = 2
+    
+    def __init__(self, view, model):
+        self.view = view
+        self.model = model
+        self.view.ConnectToModel(model)
+
+        # Inputs comes from the view
+        self.view.add_watcher('mouse-button', self.OnMouseButton)
+        self.view.add_watcher('mouse-motion', self.OnMouseMotion)
+        self.view.add_watcher('rawkey', self.OnKey)
+
+        self._mode = DrawControler.MODE_IDLE
+        self._rec = Recorder() # XXX: to use, one day ;-)
+
+    def OnMouseButton(self, evt):
+        if self._mode == DrawControler.MODE_IDLE:
+            if not evt.InObject: return
+            if evt.Key == IECODE_LBUTTON:
+                if not evt.Up:
+                    self.StartAction(DrawControler.MODE_DRAW, evt)
+                    return MUI_EventHandlerRC_Eat
+            elif evt.Key == IECODE_MBUTTON:
+                if not evt.Up:
+                    self.StartAction(DrawControler.MODE_DRAG, evt)
+                    return MUI_EventHandlerRC_Eat
+        elif self._mode == DrawControler.MODE_DRAW:
+            if evt.Key == IECODE_LBUTTON:
+                if evt.Up:
+                    self.ConfirmAction(DrawControler.MODE_IDLE, evt)
+                    return MUI_EventHandlerRC_Eat
+        elif self._mode == DrawControler.MODE_DRAG:
+            if evt.Key == IECODE_MBUTTON:
+                if evt.Up:
+                    self.ConfirmAction(DrawControler.MODE_IDLE, evt)
+                    return MUI_EventHandlerRC_Eat
+            elif evt.Key == IECODE_RBUTTON:
+                if not evt.Up:
+                    self.CancelMode(DrawControler.MODE_IDLE, evt)
+                    return MUI_EventHandlerRC_Eat
+
+    def OnMouseMotion(self, evt):
+        self._on_motion(evt)
+        self.mx = evt.MouseX
+        self.my = evt.MouseY
+        self.secs = evt.Seconds
+        self.mics = evt.Micros
+        
+    def OnKey(self, evt):
+        if evt.Up:
+            cb = self.KEYMAPS.get(evt.Key)
+            if cb:
+                cb(self, evt)
+
+    def SetMode(self, mode):
+        if mode != self._mode:
+            if mode == DrawControler.MODE_DRAG:
+                self._on_motion = self.DragOnMotion
+            elif mode == DrawControler.MODE_DRAW:
+                self._on_motion = self.DrawOnMotion
+            else:
+                self._on_motion = None # Guard
+
+            self.view.EnableMouseMoveEvents(mode != DrawControler.MODE_IDLE)
+            self._mode = mode
+        
+    mode = property(fget=lambda self: self._mode, fset=SetMode)
+
+    def StartAction(self, mode, evt):
+        # /!\ event MouseX/Y origin is the left/top origin of the window
+        self.mx = evt.MouseX
+        self.my = evt.MouseY
+        self.secs = evt.Seconds
+        self.mics = evt.Micros
+
+        # Tablet position available only during move
+        self.tbx = None
+        
+        if mode == DrawControler.MODE_DRAW:
+            pos = self.view.GetSurfacePos(self.mx, self.my)
+            self.model.InitializeDrawAction(pos)
+        elif mode == DrawControler.MODE_DRAG:
+            self.view.StartMove()
+
+        self.mode = mode 
+        
+    def CancelAction(self, mode, evt):
+        if self._mode == DrawControler.MODE_DRAG:
+            self.view.CancelMove()
+        elif self._mode == DrawControler.MODE_DRAW:
+            self.model.FinalizeDrawAction(False)
+        self.mode = mode
+
+    def ConfirmAction(self, mode, evt):
+        if self._mode == DrawControler.MODE_DRAW:
+            self.model.FinalizeDrawAction(True)
+        self.mode = mode
+
+    def DragOnMotion(self, evt):
+        # Raster moves never use tablet data, only mouse (pixel unit)
+        self.view.Scroll(self.mx - evt.MouseX, self.my - evt.MouseY)
+        self.view.RedrawDamaged()
+
+    def DrawOnMotion(self, evt):
+        # create the stroke data
+        if evt.ValidTD:
+            #if self.tbx is not None:
+            #    speed = (evt.td_NormTabletX - self.tbx,
+            #             evt.td_NormTabletY - self.tby)
+            #    self.tbx = evt.td_NormTabletX
+            #    self.tby = evt.td_NormTabletY
+            p = float(evt.td_Tags.get(TABLETA_Pressure, PRESSURE_MAX/2)) / PRESSURE_MAX
+        else:
+            p = 0.5
+
+        pos = self.view.GetSurfacePos(evt.MouseX, evt.MouseY)
+        
+        # record and render the stroke
+        # TODO: for now, a stroke is just a dict object.
+        # we need to use a custom object later for optimizations.
+        time = (evt.Seconds+evt.Micros*1e-6) - (self.secs+self.mics*1e-6)
+        stroke = dict(pos=pos, pressure=p, time=time)
+        self.model.RecordStroke(stroke)
+        self.view.AddDamagedBuffer(self.model.RenderStroke(stroke))
+        self.view.RedrawDamaged()
+
+    def OnMouseWheelUp(self, evt):
+        pass
+
+    def OnMouseWheelDown(self, evt):
+        pass
+
+    def Clear(self):
+        self.model.Clear()
+        self.view.RedrawFull()
+
+    KEYMAPS = { NM_WHEEL_UP:     OnMouseWheelUp,
+                NM_WHEEL_DOWN:   OnMouseWheelDown,
+                }
+
+    def LoadImage(self, filename):
+        assert "METHOD" is "NOT IMPLEMENTED"
+    
+    def SaveImage(self, filename):
+        im = self.model.AsPILImage('RGBA')
+        ext = os.path.splittext(filename)[1].lower()
+        dpi = (self.model.info['ResolutionX'],
+               self.model.info['ResolutionY'])
+
+        if ext == '.png':
+            im.save(filename, 'PNG', optimize=True, dpi=dpi)
+        elif ext in ('.jpg', '.jpeg'):
+            im.save(filename, 'JPEG', optimize=True, dpi=dpi, quality=90)
+        else:
+            im.save(filename)
+
+    def LoadBackground(self, filename):
+        self.model.LoadBackground(filename)
